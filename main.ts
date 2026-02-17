08fd5a6f7429993d5035cec910c61c8c8601fb02
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-02-17 13:13:45 -0500

    changed the calc formatting

--- main.ts
+++ main.ts
@@ -4,10 +4,12 @@
  * Created on: Sep 2020
  * This program calculates area and perimeter. 
 */
+
 // perimeter code 
 basic.showString('A rectangle has the dimesions of 5 and 3cm')
 basic.showString("Perimeter: 2(5 + 3) = " + (2 * (5 + 3)) + " cm")
 basic.pause(1000)
+
 //area code 
 basic.showString('A rectangle has the dimesions of 5 and 3cm')
-basic.showString('Area: 5x3 =' + (5*3) + 'cm^2' )
+basic.showString('Area: 5x3 =' + (5 * 3) + ' cm^2' )
