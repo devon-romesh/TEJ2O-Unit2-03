8befa61243a3e61ca781a8d5b9ba0e72ec55865b
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-02-17 13:05:47 -0500

    Programed basic calc (short for calculations) and changed the read me.

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
+ * Created by: Devon
  * Created on: Sep 2020
- * This program ...
+ * This program calculates area and perimeter. 
 */
-
-basic.showString('Hello, World!')
+// perimeter code 
+basic.showString('A rectangle has the dimesions of 5 and 3cm')
+basic.showString("Perimeter: 2(5 + 3) = " + (2 * (5 + 3)) + " cm")
+basic.pause(1000)
+//area code 
+basic.showString('A rectangle has the dimesions of 5 and 3cm')
+basic.showString('Area: 5x3 =' + (5*3) + 'cm^2' )
